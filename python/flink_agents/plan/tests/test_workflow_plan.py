821b6541f3e00a3d3c1a18affd691d9453d1bd2c
WenjinXie <wenjin272@gmail.com>
2025-06-10 17:23:16 +0800

    fix license check and import path.

--- python/flink_agents/plan/tests/test_workflow_plan.py
+++ python/flink_agents/plan/tests/test_workflow_plan.py
@@ -1,7 +1,25 @@
-from python.flink_agents.api.event import InputEvent, OutputEvent
-from python.flink_agents.plan.action import Action
-from python.flink_agents.plan.function import Function
-from python.flink_agents.plan.workflow_plan import WorkflowPlan
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+#################################################################################
+
+from flink_agents.api.event import InputEvent, OutputEvent
+from flink_agents.plan.action import Action
+from flink_agents.plan.function import Function
+from flink_agents.plan.workflow_plan import WorkflowPlan
 
 
 def increment(event: InputEvent) -> OutputEvent: # noqa: D103
@@ -27,7 +45,3 @@ def test_simplest_workflow_plan() -> None: # noqa: D103
             assert result_event.isLegal
             print(result_event.result)
             assert result_event.result == "2"
-
-
-if __name__ == "__main__":
-    test_simplest_workflow_plan()
