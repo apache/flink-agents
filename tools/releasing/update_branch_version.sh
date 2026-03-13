a321c292c662abf771b7e7a925b7300fbfc0345e
sxnan <suxuannan95@gmail.com>
2026-03-13 15:40:57 +0800

    [infra] Download JARs from Maven Central during pip install

--- tools/releasing/update_branch_version.sh
+++ tools/releasing/update_branch_version.sh
@@ -56,6 +56,9 @@ cd ..
 cd python
 perl -pi -e "s#^version = \".*\"#version = \"${NEW_VERSION}\"#" pyproject.toml
 perl -pi -e "s#-SNAPSHOT#\\.dev0#" pyproject.toml
+# Update jar_manifest.json version (strip -SNAPSHOT suffix for Maven Central)
+MANIFEST_VERSION=$(echo "${NEW_VERSION}" | sed 's/-SNAPSHOT//')
+perl -pi -e "s#\"version\": \".*\"#\"version\": \"${MANIFEST_VERSION}\"#" jar_manifest.json
 cd ..
 
 git commit -am "Update version to $NEW_VERSION"
