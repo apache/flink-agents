af72ae0869697ec6722178ac988bbccf12b5f055
daken <2844775971@qq.com>
2026-03-22 14:51:41 +0800

    [test]ResourceCheckTest add Apache licence

--- e2e-test/flink-agents-end-to-end-tests-integration/src/test/java/org/apache/flink/agents/integration/test/ResourceCheckTest.java
+++ e2e-test/flink-agents-end-to-end-tests-integration/src/test/java/org/apache/flink/agents/integration/test/ResourceCheckTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.flink.agents.integration.test;
 
 import org.apache.flink.agents.api.resource.ResourceName;
@@ -11,7 +28,8 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Verify that the Java class name defined in {@link ResourceName} corresponds to a class that exists.
+ * Verify that the Java class name defined in {@link ResourceName} corresponds to a class that
+ * exists.
  */
 public class ResourceCheckTest {
 
@@ -24,8 +42,9 @@ public class ResourceCheckTest {
         collectAndCheckJavaClasses(ResourceName.class, "", false, missing);
 
         assertThat(missing)
-                .as("The following Java class does not exist in ResourceName, please check the class name or module dependencies: %s"
-                        , missing)
+                .as(
+                        "The following Java class does not exist in ResourceName, please check the class name or module dependencies: %s",
+                        missing)
                 .isEmpty();
     }
 
